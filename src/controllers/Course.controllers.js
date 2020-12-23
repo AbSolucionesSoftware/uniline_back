821db906ca703a23f9684978adebd6be0e0fb81c
Diego <consultoresartificialbrain@gmail.com>
2020-12-23 11:09:50 -0600

    agregando rutas del user

--- src/controllers/Course.controllers.js
+++ src/controllers/Course.controllers.js
@@ -1,8 +1,30 @@
 const courseCtrl = {};
 
+courseCtrl.getCourses = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(505).json({message: "Error del servidor",error});
+        console.log(error);
+    }
+}
+
+courseCtrl.getCourse = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(505).json({message: "Error del servidor",error});
+        console.log(error);
+    } 
+}
 
 courseCtrl.createCourse = async (req,res) => {
-
+    try {
+        
+    } catch (error) {
+        res.status(505).json({message: "Error del servidor",error});
+        console.log(error);
+    }
 }
 
 module.exports = courseCtrl;
