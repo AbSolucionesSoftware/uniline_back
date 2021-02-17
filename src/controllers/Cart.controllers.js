aaec3302ca2adbd3da27c5f375f3e79e0eecf73d
Diego <consultoresartificialbrain@gmail.com>
2021-02-17 11:49:27 -0600

    agregando curso a carrito

--- src/controllers/Cart.controllers.js
+++ src/controllers/Cart.controllers.js
@@ -9,7 +9,7 @@ cartCtrl.aggregateCourse = async (req, res) => {
         if(userBase){
             await modelCart.updateOne(
                 {
-                    idUser: userBase.idUser._id
+                    _id: userBase._id
                 },
                 {
                     $addToSet: {
@@ -22,7 +22,7 @@ cartCtrl.aggregateCourse = async (req, res) => {
                 },
                 async (err, response) => {
                     if(err){
-                        res.status(500).json({ message: 'Ups, algo al agregar curso.', err });
+                        res.status(500).json({ message: 'Ups, algo paso al agregar curso.', err });
                     } else {
                         if(!response){
                             res.status(404).json({ message: 'Error al agregar.' });
@@ -44,7 +44,35 @@ cartCtrl.aggregateCourse = async (req, res) => {
 
 cartCtrl.deleteCourse = async (req, res) => {
     try {
-        
+        const userBase = await modelCart.findOne({idUser: req.params.idUser});
+        if(userBase){
+            await modelCart.updateOne(
+                {
+                    _id: userBase._id
+                },
+                {
+                    $pull: {
+                        courses: {
+                            course: req.params.idCurse
+                        }
+                    }
+                },
+                async (err, response) => {
+                    if(err){
+                        res.status(500).json({ message: 'Ups, algo paso al eliminar curso.', err });
+                    } else {
+                        if(!response){
+                            res.status(404).json({ message: 'Error al eliminar.' });
+                        }else{
+                            res.status(200).json({ message: 'Curso eliminado.' });
+                        }
+                    }
+                }
+            )
+        }else{
+            res.status(404).json({ message: 'Usuario no encontrado.' });
+        }
+
     } catch (error) {
         res.status(500).json({ message: error });
         console.log(error);
@@ -53,7 +81,7 @@ cartCtrl.deleteCourse = async (req, res) => {
 
 cartCtrl.getCartCourse = async (req, res) => {
     try {
-        const cartUser = await modelCart.find({idUser: req.params.idUser}).populate('idUser').populate({ path: 'courses.course', model: "course" });
+        const cartUser = await modelCart.findOne({idUser: req.params.idUser}).populate('idUser').populate({ path: 'courses.course', model: "course" });
         console.log(cartUser);
         res.status(200).json(cartUser);
     } catch (error) {
@@ -64,7 +92,7 @@ cartCtrl.getCartCourse = async (req, res) => {
 
 cartCtrl.deleteCart = async (req,res) => {
     try {
-        
+        const userBase = await modelCart.findOne()
     } catch (error) {
         res.status(500).json({ message: error });
         console.log(error);
--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -2,8 +2,10 @@ const {Router} = require('express');
 const router = Router();
 const auth = require('../middleware/auth');
 
-const { aggregateCourse,getCartCourse,deleteCourse } = require('../controllers/Cart.controllers');
+const { aggregateCourse,getCartCourse,deleteCourse, deleteCart } = require('../controllers/Cart.controllers');
 
-router.route('/:idUser').post(auth,aggregateCourse).get(getCartCourse);
+router.route('/:idUser').post(auth,aggregateCourse).get(getCartCourse).delete(deleteCart);
+
+router.route('/:idUser/delete/:idCurse').delete(deleteCourse);
 
 module.exports = router;
