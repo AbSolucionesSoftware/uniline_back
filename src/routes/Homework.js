6af4792cb314620bca3225a1cca37b77707b6f50
Diego <consultoresartificialbrain@gmail.com>
2021-03-02 13:58:15 -0600

    agregando todo

--- src/routes/Homework.js
+++ src/routes/Homework.js
@@ -2,12 +2,14 @@ const {Router} = require('express');
 const router = Router();
 const auth = require('../middleware/auth');
 
-const {uploadFile, createHomework, getHomeworks, qualificationHomeworkteacher } = require('../controllers/Homework.controllers'); 
+const {uploadFile, createHomework, getHomeworks, qualificationHomeworkteacher, deleteHomeworks } = require('../controllers/Homework.controllers'); 
 
 router.route('/:idCourse/user/:idUser').post(auth,uploadFile,createHomework);
 
 router.route('/:idCourse').get(auth,getHomeworks);
 
 router.route('/qualification/:idHomework').put(auth,qualificationHomeworkteacher);
+
+router.route('/delete/:idHomework').delete(auth,deleteHomeworks);
 
 module.exports = router;
