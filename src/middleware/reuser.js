3ac47986ef2a9db4686c756ab86c13bf9bdc926b
Diego <consultoresartificialbrain@gmail.com>
2021-03-06 12:07:12 -0600

    agregando todo

--- src/middleware/reuser.js
+++ src/middleware/reuser.js
@@ -15,7 +15,7 @@ ctrlFuncticon.generateNumCertifictate = async (length) => {
   var character = "1234567890";
   var charactersLength = character.length;
   for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += character.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
