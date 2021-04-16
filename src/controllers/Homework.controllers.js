5aee35b1cc43c72a474fc59ce21013e498eca01d
Diego <consultoresartificialbrain@gmail.com>
2021-04-16 12:58:22 -0500

    agregandot do

--- src/controllers/Homework.controllers.js
+++ src/controllers/Homework.controllers.js
@@ -22,6 +22,7 @@ homeworkCtrl.createHomework = async (req,res) => {
         /* const { qualificationHomework } = req.body;  */
         const createHomework = new modelHomework();
         console.log("LLego a homwork");
+        console.log(req.body);
         console.log(req.file);
         if(req.file){
             createHomework.homeworkFileKey = req.file.key;
